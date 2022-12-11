8b3c1c8e827001b30e8e93073639430996a56c7d
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-12-11 13:09:38 -0300

    connecting to mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
 const port = 3000;
 
 // connect to mogoDB
-const dbURI = 'mongodb+srv://EyzRyder:fzo2URQ537@nodetus.8adorfl.mongodb.net/note-tuts?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://EyzRyder:fzo2URQ537@nodetus.8adorfl.mongodb.net/node-tuts?retryWrites=true&w=majority';
 mongoose.set("strictQuery", true);
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => {
@@ -30,19 +31,47 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'))
 app.use(morgan('dev'));
 
-app.get('/add-blog', (req, res) => {
-    //
-});
+
+// mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'New Blog 2',
+//         snippet: 'about it , i know creative',
+//         body: 'even more about it, great'
+//     });
+
+//     blog.save()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then((result) => { 
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         });
+// });
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('6395f7ce4ee491154a150da5')
+//         .then((result) => { 
+//             res.send(result);
+//         })
+//         .catch((err) => { 
+//             console.log(err);
+//         });
+// });
 
 app.get('/', function (req, res) {
     // res.send('<p>Home Page</p>');
-
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    ];
-    res.render('index', { title: 'Home', blogs })
+    res.redirect('/blogs');
 });
 
 app.get('/about', function (req, res) {
@@ -52,7 +81,20 @@ app.get('/about', function (req, res) {
 
 });
 
-// redirects
+//  blog routes
+
+app.get('/blogs', (req, res) => {
+
+
+        Blog.find().sort({ createdAt: -1})
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs:result });
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
+
 app.get('/blogs/create', (req, res) => {
     res.render('create',{ title: 'Create a New Blog' })
 });
